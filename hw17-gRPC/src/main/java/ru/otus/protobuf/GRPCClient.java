adb16e237decc395b1c9a35cf4d3edebf70c8832
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2023-04-18 12:46:26 +0300

    add Deque

--- hw17-gRPC/src/main/java/ru/otus/protobuf/GRPCClient.java
+++ hw17-gRPC/src/main/java/ru/otus/protobuf/GRPCClient.java
@@ -5,16 +5,20 @@ import io.grpc.stub.StreamObserver;
 import ru.otus.protobuf.generated.ClientRequest;
 import ru.otus.protobuf.generated.RemoteDBServiceGrpc;
 import ru.otus.protobuf.generated.ServerResponse;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 
 public class GRPCClient {
 
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 8080;
-    private static int valueFromServer = 0;
-
+    private static final Deque<Integer> responseQueue = new ArrayDeque<>();
     private static final int REQUEST_FIRST_NUMBER = 0;
     private static final int REQUEST_LAST_NUMBER = 20;
+    private static final int NUMBER_OF_ITERATIONS = 50;
     public static void main(String[] args) {
 
         var channel = ManagedChannelBuilder.forAddress(SERVER_HOST,SERVER_PORT)
@@ -31,10 +35,14 @@ public class GRPCClient {
 
         var responseObserver = new StreamObserver<ServerResponse>(){
 
+            public Integer getNumberFromServer (){
+                return Optional.ofNullable(responseQueue.poll()).orElse(0);
+            }
+
             @Override
             public void onNext(ServerResponse value) {
-                valueFromServer = value.getNextNumbers();
-                System.out.printf("          Сервер---> valueFromServer: %d %n", valueFromServer);
+                responseQueue.add(value.getNextNumbers());
+                System.out.printf("          Сервер---> valueFromServer: %d %n", value.getNextNumbers());
             }
 
             @Override
@@ -52,10 +60,9 @@ public class GRPCClient {
 
         int currentValue = 0;
 
-        for (int i = 0; i < 50; i++) {
-            currentValue = currentValue + valueFromServer + 1;
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            currentValue = currentValue + responseObserver.getNumberFromServer() + 1;
             System.out.printf("%d) Клиент---> currentValue: %d%n", i, currentValue);
-            valueFromServer = 0;
             try {
                 Thread.sleep(1000);
             }catch (InterruptedException ex){
